858d078ae201c96a05b4c325b288aec4cb47dd72
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-01-11 23:14:30 +0430

    .... refactoring

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { useMemo, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+import Todos from "./Todos";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Todos />);
