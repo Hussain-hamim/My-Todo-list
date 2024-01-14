7fe38006a1bc9da9e276620c8b856d8343796840
Hussain Hamim <94266855+Hussain-hamim@users.noreply.github.com>
2024-01-14 07:15:20 +0430

    Update Todos.js

--- src/Todos.js
+++ src/Todos.js
@@ -81,7 +81,7 @@ const Todos = () => {
           }}
           onClick={addTodo}
         >
-          Add Todos
+          Add Todo
         </button>
       </div>
 
@@ -147,7 +147,7 @@ const Todos = () => {
 
       <footer style={{ marginTop: "70px" }}>
         <div style={{ opacity: "0.6", fontFamily: "Arial", fontSize: "15px" }}>
-          @Copyright Hussain Hamim 2024
+          ©Copyright 2024 Hussain Hamim
         </div>
         <div></div>
       </footer>
