2e8dddadff45620c10dd2acdee556834053b2e27
Hussain Hamim <94266855+Hussain-hamim@users.noreply.github.com>
2024-02-08 23:53:15 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import Todo from "./Todo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Todo />
   </BrowserRouter>
 );
